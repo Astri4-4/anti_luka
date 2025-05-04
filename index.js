39b90d5fc17c6148cad12c8a4ed11440695601f5
Astria <sacha.guerin@supinfo.com>
2025-05-04 15:40:08 +0200

    ADD multi server system

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import os from "node:os";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
+import dns from "node:dns";
+
+const interfaces = os.networkInterfaces();
+const ip = getHostname();
 
 const io = new Server({
     cors: {
-        origin: "0.0.0.0/24"
+        origin: "*"
     }
 })
 
@@ -11,14 +15,17 @@ io.on("connection", (socket) => {
     console.log(socket.id);
 });
 
+console.log(os.hostname());
+
 checkOverloaded();
 
 setInterval(() => {
-    const free = os.freemem()/1024/1024/1024;
-    const total = os.totalmem()/1024/1024/1024;
+    const free = os.freemem() / 1024 / 1024 / 1024;
+    const total = os.totalmem() / 1024 / 1024 / 1024;
     const cpus = os.cpus();
 
     const payload = {
+        hostname: os.hostname(),
         cpus: cpus,
         total: total,
         free: free
@@ -26,18 +33,30 @@ setInterval(() => {
 
     checkOverloaded();
 
-    io.emit("hardware", payload);
+    io.emit(ip + "-hardware", payload);
 
 }, 1000);
 
 function checkOverloaded() {
-    const maxMemoryValue = 0.1*os.totalmem();
-    console.log(maxMemoryValue/1024/1024/1024);
+    const maxMemoryValue = 0.1 * os.totalmem();
 
     if (os.freemem() < maxMemoryValue) {
         io.emit("overloaded", "System overloaded !!!");
     }
 
+}
+
+function getHostname() {
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+                return alias.address;
+        }
+    }
+    return '0.0.0.0';
 }
 
 io.listen(3000);
